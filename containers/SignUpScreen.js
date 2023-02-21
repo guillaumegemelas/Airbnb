057a7ddc05e6533e0b49c13c2c442d4b386e1112
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-21 15:49:01 +0100

    login page commit1

--- containers/SignUpScreen.js
+++ containers/SignUpScreen.js
@@ -1,9 +1,42 @@
-import { Button, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import logo from "../assets/logo.png";
 
 export default function SignUpScreen({ setToken }) {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConf, setPasswordConf] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   return (
     <View>
       <View>
+        <View style={styles.logo}>
+          <Image
+            source={logo}
+            style={{
+              width: 90,
+              height: 90,
+
+              marginBottom: 10,
+              marginTop: 10,
+            }}
+          />
+          <View>
+            <Text style={styles.txtlogo}>Sign up</Text>
+          </View>
+        </View>
+
         <Text>Name: </Text>
         <TextInput placeholder="Username" />
         <Text>Password: </Text>
@@ -19,3 +52,11 @@ export default function SignUpScreen({ setToken }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    height: "30%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
